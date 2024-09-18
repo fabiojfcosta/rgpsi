f975ea89fb565fea0bcaa1e586aea76874547f53
fjfc <fabiojfcosta.31@gmail.com>
2024-09-18 17:55:44 +0100

    responsive, navbar colors

--- src/app/layout/menubar/menubar.component.ts
+++ src/app/layout/menubar/menubar.component.ts
@@ -10,11 +10,12 @@ import { ViewportScroller } from '@angular/common';
 })
 export class MenubarComponent {
   public items: MenuItem[] | undefined;
+  public itemsResponsive: MenuItem[] | undefined;
 
   ngOnInit() {
       this.items = [
         {
-          label: 'Introdução',
+          label: 'Início',
           command: () => this.scroller.scrollToPosition([0,0])
       },  
       {
@@ -31,14 +32,34 @@ export class MenubarComponent {
       },
       {
         label: 'Contactos',
-        command: () => this.scroller.scrollToAnchor("contacts")
+        command: () => this.onScrollToAnchor("contacts")
       },
       {
-        label: 'Mande uma mensagem sem compromissos',
+        label: 'Mande uma mensagem',
         style: {'margin-left': 'auto'},
-        icon: 'pi pi-whatsapp'
+        icon: 'pi pi-whatsapp',
+        command: () => this.openWhatsapp()
       }
     ];
+
+    this.itemsResponsive = [
+      {
+        label: 'Início',
+        style: {'padding': '2px'},
+        command: () => this.scroller.scrollToPosition([0,0])
+    },  
+    {
+      label: 'Contactos',
+      command: () => this.onScrollToAnchor("contacts")
+    },
+    {
+      label: 'Mande uma mensagem',
+      style: {'margin-left': 'auto'},
+      icon: 'pi pi-whatsapp',
+      command: () => this.openWhatsapp()
+    }
+  ];
+
   }
 
   constructor(private scroller: ViewportScroller) {}
@@ -47,5 +68,7 @@ export class MenubarComponent {
     this.scroller.scrollToAnchor(id)
   }
    
-
+  openWhatsapp(){
+    window.open("https://api.whatsapp.com/send?phone=926223656", "_blank");
+  }
 }
